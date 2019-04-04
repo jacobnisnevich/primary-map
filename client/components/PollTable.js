bd003b16cf192fb4b7aab71d5588d6935a30d951
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2019-04-04 00:47:41 -0700

    feat(): filter out empty candidates for recent polls

--- client/components/PollTable.js
+++ client/components/PollTable.js
@@ -1,18 +1,31 @@
 import React, { Component, Fragment } from 'react';
-import { startCase, isEmpty } from 'lodash';
+import { startCase, isEmpty, pick } from 'lodash';
 
 import './PollTable.css';
 
 import { fixMessedUpName, getColumnFormatter } from '../utils/common';
 
 export default class PollTable extends Component {
+  filterOutEmptyColumns = mostRecentPollData => {
+    const columns = Object.keys(mostRecentPollData[0]);
+
+    const validColumns = columns.filter(column => {
+      const valuesForColumn = mostRecentPollData.map(poll => poll[column]);
+      return valuesForColumn.filter(value => value !== 0).length > 0;
+    });
+
+    return mostRecentPollData.map(poll => pick(poll, validColumns));
+  };
+
   getTableHead = () => {
     const { mostRecentPollData } = this.props;
+
+    const filteredPollData = this.filterOutEmptyColumns(mostRecentPollData);
 
     return (
       <thead>
         <tr>
-          {Object.keys(mostRecentPollData[0]).map((header, index) => (
+          {Object.keys(filteredPollData[0]).map((header, index) => (
             <th key={index}>{fixMessedUpName(startCase(header))}</th>
           ))}
         </tr>
@@ -23,9 +36,11 @@ export default class PollTable extends Component {
   getTableBody = () => {
     const { mostRecentPollData } = this.props;
 
+    const filteredPollData = this.filterOutEmptyColumns(mostRecentPollData);
+
     return (
       <tbody>
-        {mostRecentPollData.map((poll, rowIndex) => {
+        {filteredPollData.map((poll, rowIndex) => {
           const columns = Object.values(poll);
           const candidateColumns = columns.slice(4, columns.length - 1);
           const winningCandidateIndex = columns.indexOf(Math.max([candidateColumns]));
